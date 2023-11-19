84c797ca1c99fd106e1ed10087102079a2e64fdf
Juan David Chimbaco <dajun318@gmail.com>
2023-11-19 18:54:56 -0500

    changes first

---
+++ Arata_app/auth/usuario.py
@@ -0,0 +1,13 @@
+
+from django.db import models
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
