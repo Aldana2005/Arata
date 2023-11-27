d1206abb722879d17a558534e60d73e70f4e4eb9
Juan.Chimbaco <dajun318@gmail.com>
2023-11-27 10:03:31 -0500

    fonts

--- Arata_app/auth/urls.py
+++ Arata_app/auth/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # Otras URLS...
-
     # Registro
     path('registro/', views.registro, name='registro'),
-
-    # Inicio de sesión
-    path('inicio_sesion/', auth_views.LoginView.as_view(template_name='usuarios/inicio_sesion.html'), name='inicio_sesion'),
-
-    # Cierre de sesión
-    path('cerrar_sesion/', auth_views.LogoutView.as_view(), name='cerrar_sesion'),
     path('login/', views.login , name='login'),
+    path('loginAuth/', views.loginAuth, name='loginAuth'),
 ]
--- Arata_app/auth/views.py
+++ Arata_app/auth/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from .forms import UsuarioRegistroForm
 
 def registro(request):
@@ -15,3 +15,14 @@ def registro(request):
 
 def login(request):
     return render(request, 'auth/login.html', {})
+
+def loginAuth(request):
+    user_name = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=user_name, password=password)
+    if user is not None:
+        login(request,user)
+        return redirect('dashboard')
+    else:
+        print(user_name,password)
+        return redirect('login')
